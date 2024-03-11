3769e8e16e275eed2e5453961a92ec32849d6a60
Martin Chakarov <martin.d.chakarov@gmail.com>
2024-03-11 19:58:57 +0200

    Create dim_customers

---
+++ models/marts/dim_customers.sql
@@ -0,0 +1,15 @@
+WITH BASE_DATA AS (
+    SELECT * FROM {{ ref('stg_customers') }}
+),
+
+FINAL AS (
+    SELECT
+        ID AS CUSTOMER_ID,
+        FIRST_NAME || ' ' || LAST_NAME AS NAME,
+        EMAIL,
+        CASE WHEN GENDER NOT IN ('Male', 'Female') THEN 'Other' ELSE GENDER END AS GENDER,
+        IP_ADDRESS
+    FROM {{ ref('stg_customers') }}
+)
+
+SELECT * FROM FINAL
