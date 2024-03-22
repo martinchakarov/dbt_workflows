ee88548c090018996ad4da50eb88a3226eae9699
Martin Chakarov <martin.d.chakarov@gmail.com>
2024-03-22 12:02:43 +0200

    Add project variables for ingestion warehouse status check

--- macros/utils/check_ingestion_warehouse_status.sql
+++ macros/utils/check_ingestion_warehouse_status.sql
@@ -1,6 +1,18 @@
 {% macro check_ingestion_warehouse_status(ingestion_warehouse = env_var("INGESTION_WAREHOUSE")) %}
 
-  {{ log("Checking if the ingestion warehouse is active...", info = True) }}
+  {{ log("Checking the state of the ingestion warehouse...", info = True) }}
+  {% set freshness_condition = True if var('check_ingestion_timestamp') else False %}
+  {% set status_condition = True if var('check_ingestion_status') else False %}
+
+  {{ log('Will check for the following criteria:', info=True) }}
+
+  {% if freshness_condition %}
+    {{ log('- The ingestion warehouse was last resumed less than ' ~ var('max_data_age_in_minutes') ~ ' minutes ago.', info=True) }}
+  {% endif %}
+  
+  {% if status_condition %}
+    {{ log('- The ingestion warehouse is currently suspended.', info=True) }}
+  {% endif %}
 
   {# Run a query to check the current status of the Snowflake warehouse, as well as when it was last resumed #}
   {% set main_query = "SHOW WAREHOUSES LIKE '" ~ ingestion_warehouse | upper ~ "';"%}
@@ -18,11 +30,12 @@
     {{ log("The current state of the ingestion warehouse is " ~ state ~ ".", info = True) }}
     {{ log("The ingestion warehouse was last resumed at " ~ last_resumed_local_tz.strftime('%Y-%m-%d %H:%M:%S') ~ ". ("~ diff | int ~ " minutes ago)", info = True)}}
 
-    {% if diff > 120 %}
-        {{ exceptions.raise_compiler_error('The ingestion warehouse has not been active for more than 2 hours. Aborting...') }}
-    {% elif state != 'SUSPENDED' %}
-        {{ exceptions.raise_compiler_error('The ingestion warehouse is still active. Aborting...') }}
+    {% if freshness_condition and diff > var('max_data_age_in_minutes') %}
+        {{ exceptions.raise_compiler_error('The ingestion warehouse has been inactive for more than '~ var('max_data_age_in_minutes') ~ ' minutes' ~'.') }}
+    {% elif status_condition and state != 'SUSPENDED' %}
+        {{ exceptions.raise_compiler_error('The ingestion warehouse is still active.') }}
     {% endif %}
-        {{ log("All criteria have been met. Proceeding with next steps.", info = True)}}
+
+    {{ log("All criteria have been met. Proceeding with next steps.", info = True)}}
   {% endif %}
 {% endmacro %}
